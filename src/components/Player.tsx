dc1db97835ed238036fde5cd4b59292d8b724c75
Kranti Kumar Shah <Krantikumar803@gmail.com>
2025-07-14 16:56:21 +0545

    added the song play functionality

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -5,16 +5,115 @@ import {
   Volume2,
   Repeat,
   Shuffle,
+  Play,
+  VolumeX,
 } from "lucide-react";
 import song1 from "../assets/song1.png";
+import dhinkachika from "../assets/Amrita Kak - Dhinka Chika (From  Ready ).mp3";
+import savanAayaHai from "../assets/Arijit Singh - Sawan Aaya Hai (From  Creature 3D ).mp3";
+import mainAgarKahoon from "../assets/Main Agar Kahoon Om Shanti Om Shahrukh Khan,Deepika Padukone Sonu Nigam,Shreya Ghosal.mp3";
+import jaadoKiJhappi from "../assets/Mika Singh - Jadoo Ki Jhappi.mp3";
+import ratanLambiyan from "../assets/Raataan Lambiyan - Shershaah Sidharth – Kiara Tanishk B. Jubin Asees.mp3";
+import saathiyaa from "../assets/Shreya Ghoshal - Saathiyaa (From  Singham ).mp3";
+
+import { useEffect, useRef, useState } from "react";
+
+//songs array
+const songs = [
+  {
+    name: "Dhinka Chika",
+    src: dhinkachika,
+  },
+  {
+    name: "Savan Aaya Hai",
+    src: savanAayaHai,
+  },
+  {
+    name: "Main Agar Kahoon",
+    src: mainAgarKahoon,
+  },
+  {
+    name: "Jaado Ki Jhappi",
+    src: jaadoKiJhappi,
+  },
+  {
+    name: "Ratan Lambiyan",
+    src: ratanLambiyan,
+  },
+  {
+    name: "Saathiyaa",
+    src: saathiyaa,
+  },
+];
 
 const MusicPlayer = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isRepeat, setIsRepeat] = useState(false);
+  const [isMute, setIsMute] = useState(false);
+  const [autoPlay, setAutoplay] = useState(false);
+  const [currentSong, setCurrentSong] = useState(0);
+  // const [currentSongDuration, setCurrentSongDuration] = useState();
+
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+  const currentSongRef = useRef(0);
+
+  const handlePlay = () => {
+    if (!isPlaying) {
+      setIsPlaying(true);
+      audioRef?.current?.play();
+    } else {
+      setIsPlaying(false);
+      audioRef?.current?.pause();
+    }
+  };
+
+  const handleNext = () => {
+    if (currentSongRef.current < songs.length - 1) {
+      currentSongRef.current += 1;
+      setIsPlaying(true);
+      setCurrentSong(currentSongRef.current);
+      setAutoplay(true);
+    }
+  };
+
+  const handlePrev = () => {
+    if (currentSongRef.current === 0) {
+      // Restart song if already at the first one
+      if (audioRef.current) {
+        audioRef.current.currentTime = 0;
+        audioRef.current.play();
+        setIsPlaying(true);
+        setAutoplay(true);
+      }
+      return;
+    }
+
+    currentSongRef.current -= 1;
+    setCurrentSong(currentSongRef.current);
+    setIsPlaying(true);
+  };
+
+  useEffect(() => {
+    const audio = audioRef.current;
+    if (!audio) return;
+    const handleSongEnded = () => setIsPlaying(false);
+    audioRef.current?.addEventListener("ended", handleSongEnded);
+    return () =>
+      audioRef.current?.removeEventListener("ended", handleSongEnded);
+  }, []);
+
   return (
     <>
       {/* audio tag */}
       <audio
-        src="/audio/test.mp3" // change this to your audio URL or file
+        // className="hidden"
+        ref={audioRef}
+        src={songs[currentSong].src} // change this to your audio URL or file
         controls
+        controlsList="nodownload"
+        loop={isRepeat}
+        muted={isMute}
+        autoPlay={autoPlay}
       />
       <div className="h-28 fixed bottom-0 left-0 right-0 bg-[#1f1f1f] text-white shadow-lg px-4 py-3 flex items-center justify-center sm:justify-between z-50">
         {/* Song Info */}
@@ -25,7 +124,9 @@ const MusicPlayer = () => {
             className="w-12 h-12 object-cover rounded-md md:block hidden"
           />
           <div>
-            <h4 className="text-sm font-semibold truncate">Sorfcore</h4>
+            <h4 className="text-sm font-semibold truncate">
+              {songs[currentSong].name}
+            </h4>
             <p className="text-xs text-gray-400">The Neighbourhood</p>
           </div>
         </div>
@@ -36,34 +137,50 @@ const MusicPlayer = () => {
             {/* Shuffle */}
             <button
               className="
-             text-[#62d962]hover:text-[#62d962]"
+             text-[#62d962]hover:text-[#62d962] cursor-pointer border-none outline-none"
             >
               <Shuffle size={18} />
             </button>
 
-            <button className="hover:text-[#62d962]">
+            <button
+              onClick={handlePrev}
+              className="hover:text-[#62d962] cursor-pointer border-none outline-none"
+            >
               <SkipBack size={20} />
             </button>
 
-            <button className="bg-[#62d962] text-white p-2 rounded-full hover:scale-105 transition">
-              <Pause size={20} />
+            <button
+              onClick={handlePlay}
+              className="bg-[#62d962] text-white p-2 rounded-full hover:scale-105 transition cursor-pointer border-none outline-none"
+            >
+              {isPlaying ? <Pause size={20} /> : <Play size={20} />}
             </button>
 
-            <button className="hover:text-[#62d962]">
+            <button
+              onClick={handleNext}
+              className="hover:text-[#62d962] cursor-pointer border-none outline-none"
+            >
               <SkipForward size={20} />
             </button>
 
             {/* Repeat */}
-            <button className="text-[#62d962]hover:text-[#62d962]">
-              <Repeat size={18} />
+            <button
+              onClick={() => setIsRepeat(!isRepeat)}
+              className="text-[#62d962]hover:text-[#62d962] cursor-pointer border-none outline-none"
+            >
+              {isRepeat ? (
+                <Repeat size={18} className="text-[#62d962]" />
+              ) : (
+                <Repeat size={18} />
+              )}
             </button>
           </div>
 
           {/* Progress bar */}
           <div className="flex items-center gap-2 w-full text-xs text-gray-400 mt-2">
             <span>0:00</span>
-            <div className="md:w-full w-5xl h-1 bg-gray-700 rounded-full">
-              <div className="h-1 bg-[#62d962] w-1/3 rounded-full" />
+            <div className="md:w-full h-1 bg-gray-700 rounded-full">
+              <div className="h-1 bg-[#62d962] w-[100%] rounded-full" />
             </div>
             <span>3:26</span>
           </div>
@@ -71,7 +188,12 @@ const MusicPlayer = () => {
 
         {/* Volume */}
         <div className="hidden sm:flex items-center gap-2 w-1/3 justify-end">
-          <Volume2 size={18} />
+          <button
+            className="border-none outline-none cursor-pointer"
+            onClick={() => setIsMute(!isMute)}
+          >
+            {isMute ? <VolumeX size={18} /> : <Volume2 size={18} />}
+          </button>
           <input
             type="range"
             min="0"
--- src/components/Songs.tsx
+++ src/components/Songs.tsx
@@ -13,36 +13,38 @@ const Songs = () => {
         </button>
       </div>
       {[...Array(5).keys()].map((_, index) => (
-        <div className="flex items-center justify-between bg-[#1f1f1f] text-white p-3 rounded-lg w-full my-4">
-          {/* Rank */}
-          <div className="text-xl font-bold w-12">#{index + 1}</div>
-
+        <div
+        key={index}
+         className="flex items-center justify-between bg-[#1f1f1f] text-white p-3 rounded-lg w-full my-4">
           {/* Album Art + Title + Artist */}
-          <div className="flex items-center gap-3 w-1/3">
-            <img
-              src={song1}
-              alt="Album"
-              className="w-12 h-12 rounded-md object-cover"
-            />
-            <div>
-              <h3 className="font-semibold">Sorfcore</h3>
-              <p className="text-xs text-gray-400">The Neighbourhood</p>
+          <div className="flex gap-x-2">
+            <span className="text-xl font-bold w-12">#{index + 1}</span>
+            <div className="flex items-center gap-3 w-1/3">
+              <img
+                src={song1}
+                alt="Album"
+                className="w-12 h-12 rounded-md object-cover"
+              />
+              <div>
+                <h3 className="font-semibold">Sorfcore</h3>
+                <p className="text-xs text-gray-400">The Neighbourhood</p>
+              </div>
             </div>
           </div>
 
           {/* Release Date */}
-          <div className="text-sm text-gray-400 w-28 text-center">
+          <div className="text-sm md:block hidden text-gray-400 w-28 text-center">
             Nov 4, 2023
           </div>
 
           {/* Album Name */}
-          <div className="text-sm text-gray-200 flex-1 text-center truncate">
+          <div className="text-sm sm:block hidden text-gray-200 flex-1 text-center truncate">
             Hard to Imagine the Neighbourhood Ever Changing
           </div>
 
           {/* Like + Duration */}
-          <div className="flex items-center gap-2 text-sm w-20 justify-end">
-            <Heart size={16} className="text-[#62d962]" />
+          <div className="flex items-center gap-4 text-sm w-20 justify-end">
+            <Heart size={17} className="text-[#62d962]" />
             <span>3:26</span>
           </div>
         </div>
